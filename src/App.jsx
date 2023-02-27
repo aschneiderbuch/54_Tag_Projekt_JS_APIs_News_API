3435972e1d07ebebb5b40bd84f8fafecbe2b4371
aschneiderbuch <aschneiderbuch2@gmail.com>
2023-02-27 14:08:02 +0100

    start

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+// import css 
+
+function App() {
+  return (
+    <div className="App">
+
+<h1 className='text-stone-700 bg-red-800'>Test
+<p>Test</p>
+</h1>
+
+    </div>
+  );
+}
+
+export default App;
